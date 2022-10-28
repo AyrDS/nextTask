06f3ff02052a2b542c7120020916f2761edb5158
AyrDS <ayrtonds97@gmail.com>
2022-10-28 16:28:56 -0300

    Login y authlayout terminado

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -4,7 +4,7 @@ export const AuthLayout = () => {
    return (
       <>
          <main className='container mx-auto mt-5 md:mt-20 p-5 md:flex md:justify-center' >
-            <div className='md:w-2/3 lg:w-2/5' >
+            <div className='md:w-3/4 lg:w-2/4' >
                <h1 className='text-center text-4xl mb-10 font-bold text-stone-800' >
                   Tus <span className='text-red-500' >proyectos</span> en un solo lugar
                </h1>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,11 @@
+import { Link } from 'react-router-dom';
 
 export const Login = () => {
    return (
       <>
          <h1 className="text-sky-600 font-black text-2xl" >Inciar Sesión</h1>
 
-         <form className="my-5 bg-white shadow rounded-lg p-10" >
+         <form className="my-5 bg-white shadow rounded-lg p-8" >
             <div className="my-5" >
                <label
                   htmlFor="email"
@@ -40,9 +41,23 @@ export const Login = () => {
                value='Iniciar Sesión'
                className="bg-sky-700 w-full py-3 mb-5 text-white uppercase font-bold rounded cursor-pointer hover:bg-sky-800 transition-all"
             />
-
-            
          </form>
+
+         <nav className="lg:flex lg:justify-between" >
+            <Link
+               to='/registro'
+               className='block text-center my-5 lg:my-2 text-slate-500 uppercase text-sm hover:text-slate-700'
+            >
+               ¿No tienes cuenta?
+            </Link>
+
+            <Link
+               to='/registro'
+               className='block text-center my-5 lg:my-2 text-slate-500 uppercase text-sm hover:text-slate-700'
+            >
+               ¿No puedes iniciar sesión?
+            </Link>
+         </nav>
       </>
    )
 }
